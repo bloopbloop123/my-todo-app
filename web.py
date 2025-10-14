58cae4347993c28f4635905c29adc4896e17ac48
bloopbloop123 <kitson145@gmail.com>
2025-10-14 20:27:43 +0100

    Initial commit

--- web.py
+++ web.py
@@ -20,7 +20,7 @@ for index, todo in enumerate(todos):
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
-        st.experimental_rerun()
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
